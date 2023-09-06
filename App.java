28cfbb1aedaec4ae5145fd262822cea38e8a811b
Aude Vernet <aude@Audes-MacBook-Air.local>
2023-09-06 01:11:56 +0100

    Added a choice of numbers exo3

--- App.java
+++ App.java
@@ -1,24 +1,20 @@
 import java.util.Scanner;  // Import the Scanner class
-import java.util.Random;
 
 public class App {
     public static void main(String[] args) {
         System.out.println("Welcome to the calculator!"); // Display the string.
-        Random random = new Random();
-        int randomNumber1 = random.nextInt(100);
-        int randomNumber2 = random.nextInt(100);
-        System.out.println("Random number 1: " + randomNumber1);
-        System.out.println("Random number 2: " + randomNumber2);
-        System.out.println("Enter an operation to perform on them: +, - or *"); // Display the string.
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first number, enter second number and an operation to perform on them: +, - or *");
+        double number1 = scanner.nextDouble();
+        double number2 = scanner.nextDouble();
         String input = scanner.next();
-        int results;
-        if (input.equals("+")) { // compare two strings use equals method as two different objs with same value
-        results = randomNumber1 + randomNumber2;
+        double results;
+        if (input.equals("+")) { // comparing two strings use equals method as two different objs with same value
+        results = number1 + number2;
         } else if (input.equals("-")) {
-            results = randomNumber1 - randomNumber2;
+            results = number1 - number2;
         } else {
-            results = randomNumber1 * randomNumber2;
+            results = number1 * number2;
         }
         System.out.println("The results is " + results);
             }
